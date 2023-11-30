42b353ee74e445a09d95c0fa2870501801dc0bbc
Renan <renansaueia@gmail.com>
2023-11-30 17:18:39 -0300

    latest changes to the project

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,20 @@
 /*   By: rsaueia- <rsaueia-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:35:58 by rsaueia-          #+#    #+#             */
-/*   Updated: 2023/11/28 20:02:17 by rsaueia-         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:12:46 by rsaueia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdarg.h>
-#include <libft.h>
 
-int	putchar_print(char c)
+int	putchar_print(int c)
 {
 	write(1, &c, 1);
 	return (1);
 }
 
-int	putstr_print(char *s)
+int	putstr_print(const char *s)
 {
 	size_t	index;
 
@@ -43,6 +43,11 @@ int	putnbr_print(long n, int base, int type)
 		symbols = "0123456789abcdef";
 	if (type == 2)
 		symbols = "0123456789ABCDEF";
+	if (type == 3)
+	{
+		symbols = "0123456789abcdef";
+		putstr_print("0x");
+	}
 	if (n < 0)
 	{
 		n = n * (-1);
@@ -68,29 +73,39 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format == '%')
+		{
 			format++;
-		else if (*format == 'c')
-			count += putchar_print(va_arg(args, const char *));
-		else if (*format == 's')
-			count += putstr_print(va_arg(args, const char *));
-		else if (*format == 'p')
-			
-		else if (*format == 'd')
-			count += putnbr(va_arg(args, (long)int n, 10));
-		else if (*format == 'i')
-			count += putnbr_print(va_arg(args, (long)int n, 10));
-		else if (*format == 'u')
-			count += putnbr_print(va_arg(args, (unsigned long)int n, 10));
-		else if (*format == 'x')
-			count += putnbr_print(va_arg(args, (long)int n, 16, 1));
-		else if (*format == 'X')
-			count += putnbr_print(va_arg(args, (long)int n, 16, 2));
-		else if (*format == '%')
-			putchar_print('%');
+			if (*format == 'c')
+				count += putchar_print(va_arg(args, int));
+			else if (*format == 's')
+				count += putstr_print(va_arg(args, const char *));
+			else if (*format == 'p')
+				count += putnbr_print(va_arg(args, long int), 16, 3);
+			else if (*format == 'd')
+				count += putnbr_print(va_arg(args, long int), 10, 1);
+			else if (*format == 'i')
+				count += putnbr_print(va_arg(args, long int), 10, 1);
+			else if (*format == 'u')
+				count += putnbr_print(va_arg(args, unsigned long int), 10, 1);
+			else if (*format == 'x')
+				count += putnbr_print(va_arg(args, long int), 16, 1);
+			else if (*format == 'X')
+				count += putnbr_print(va_arg(args, long int), 16, 2);
+			else if (*format == '%')
+				putchar_print('%');
+		}
 		else
-			ft_putchar_fd(*format);
+			putchar_print(*format);
 		format++;
 		va_end(args);
 	}
 	return (count);
+}
+
+int	main(void)
+{
+	ft_printf("Hello, my name is %s, and I am %i years old.\n", "Ezequiel", 47);
+	ft_printf("Hello, my name is %c, and I am %d years old.\n", 'E', 47);
+	ft_printf("Hello, my name is %x, and I am %X years old.\n", 42, 42);
+	ft_printf("Hello, my name is %p, and I am %X years old.\n", 42, 42);
 }
